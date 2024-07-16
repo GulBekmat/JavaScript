d65d768a76cd2a13738e9082316ed7a28ed759a5
gigibekmatova <gigibekmatova@mail.ru>
2024-07-15 18:16:34 -0700

    javascript coding M tasks

---
+++ script.js
@@ -0,0 +1,39 @@
+/*Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.*/
+
+/*Example 1:
+
+Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+Example 2:
+
+Input: intervals = [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.*/
+
+
+let input1 = [[1,3], [2,6], [8,10], [15,18]];//[[1,6], [8,10], [15,18]]
+let input2 = [[1,4], [4,5]];//[[1,5]]
+let input3 = [[11,12], [2,3], [5,7], [1,4], [8,10], [6,8]];//[[1,4], [5,10], [11,12]]
+
+function marge(intervals) {
+  if (intervals.length < 2) {
+    return intervals;
+  }
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  let result = [intervals[0]];
+
+  for (let interval of intervals) {
+    let recent = result[result.length - 1];
+    if (recent[1] >= interval[0]) {
+      recent[1] = Math.max(recent[1], interval[1]);
+    } else {
+      result.push(interval);
+    }
+  }
+  return result;
+}
+console.log(marge(input1));
+console.log(marge(input2));
+console.log(marge(input3));
